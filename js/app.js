7d74478dddaed6e3f694594806c0b0e722abe6b4
Opilane TTHK <opilane@tthk.ee>
2019-10-14 10:07:20 +0300

    profile

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 const github = new GitHub;
 const ui = new UI;
-github.getUser("Nikoljka")
+github.getUser("DaniellaKolpakova")
         .then(data => {
             if(data.profile.message === 'Not Found') {
                 ui.showAlert('User not found!', 'alert alert-danger')
